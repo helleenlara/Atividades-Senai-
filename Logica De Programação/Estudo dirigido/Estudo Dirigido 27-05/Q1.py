73eccd19992cfcd04cec3a07ecd59a7465eb2bd8
Lara Hellen Marques <138770706+helleenlara@users.noreply.github.com>
2026-05-27 12:11:09 +0000

    update

---
+++ Logica De Programação/Estudo dirigido/Estudo Dirigido 27-05/Q1.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia um número inteiro e informe se ele é par ou ímpar.
+
+n = int(input("Digite um número: "))
+
+if n%2 == 0:
+    print("Número par")
+else:
+    print("Número impar")
