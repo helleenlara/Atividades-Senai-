90add92fa111f1817fb8f2457f24ceadd51034de
Lara Hellen Marques <138770706+helleenlara@users.noreply.github.com>
2026-05-27 13:15:09 +0000

    .

---
+++ Logica De Programação/Estudo dirigido/Estudo Dirigido 27-05/Q7.py
@@ -0,0 +1,12 @@
+#Leia três números e informe o maior deles.
+
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+n3 = int(input("Digite o terceiro numero: "))
+
+if n1 > n2 and n1 > n3:
+    print("O primeiro numero é maior")
+elif n2 > n1 and n2 > n3:
+    print("O segundo numero é maior")
+else:
+    print("O terceiro numero é maior")
